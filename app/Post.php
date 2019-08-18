66e57c304d5338d6f43e7e950fa14ddd686a9d65
小林将希 <kobayashimasaki@kobayashimasakinoMacBook-Pro.local>
2019-08-18 10:46:32 +0900

    sample_board

---
+++ app/Post.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+/**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id','category_id','content', 'title'
+    ];
+    public function category(){
+        // 投稿は1つのカテゴリーに属する
+        return $this->belongsTo(\App\Category::class,'category_id');
+      }
+    
+    public function user(){
+        // 投稿は1つのカテゴリーに属する
+        return $this->belongsTo(\App\User::class,'user_id');
+      }
+
+    public function comments(){
+      // 投稿は1つのカテゴリーに属する
+      return $this->hasMany(\App\Comment::class,'post_id', 'id');
+    }
+}
---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('categories')->insert([
+            ['category_name' => 'book'],
+            ['category_name' => 'cafe'],
+            ['category_name' => 'travel'],
+            ['category_name' => 'food'],
+            ['category_name' => 'music'],
+        ]); 
+    }
+}
